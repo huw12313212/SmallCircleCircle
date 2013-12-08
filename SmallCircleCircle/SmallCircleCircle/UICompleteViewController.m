17d4d9d4539d81fe155b8639324c852efabfb9c9
王瀚宇 <huw12313212@gmail.com>
2013-12-09 01:38:01 +0800

    +Facebook Request API

--- SmallCircleCircle/SmallCircleCircle/UICompleteViewController.m
+++ SmallCircleCircle/SmallCircleCircle/UICompleteViewController.m
@@ -9,6 +9,11 @@
 #import "UICompleteViewController.h"
 #import "DBQueryInterface.h"
 #import "FakeDB.h"
+#import <FacebookSDK/FacebookSDK.h>
+#import "huwAppDelegate.h"
+#import <MessageUI/MessageUI.h>
+#import <AddressBook/AddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
 
 @interface UICompleteViewController ()
 
@@ -31,6 +36,100 @@
         // Custom initialization
     }
     return self;
+}
+
+- (IBAction)InviteClicked:(id)sender {
+    NSMutableDictionary* params =   [NSMutableDictionary dictionaryWithObjectsAndKeys: nil];
+    
+    
+    huwAppDelegate *appDelegate = [[UIApplication sharedApplication]delegate];
+
+    [FBWebDialogs
+     presentRequestsDialogModallyWithSession:appDelegate.session
+     message:[NSString stringWithFormat:@"來參加我的團購：scc://%@", self.ActivityID]
+     title:@"團購小圈圈"
+     parameters:nil
+     handler:^(FBWebDialogResult result, NSURL *resultURL, NSError *error) {
+         if (error) {
+             // Error launching the dialog or sending the request.
+             NSLog(@"Error sending request.");
+         } else {
+             if (result == FBWebDialogResultDialogNotCompleted) {
+                 // User clicked the "x" icon
+                 NSLog(@"User canceled request.");
+             } else {
+                 // Handle the send request callback
+                 NSDictionary *urlParams = [self parseURLParams:[resultURL query]];
+                 
+                 NSLog(@"%@",urlParams);
+                 
+                 if (![urlParams valueForKey:@"request"]) {
+                     // User clicked the Cancel button
+                     NSLog(@"User canceled request.");
+                 } else {
+                     // User clicked the Send button
+                     NSString *requestID = [urlParams valueForKey:@"request"];
+                     NSLog(@"Request ID: %@", requestID);
+                     
+                     
+                     
+                     NSMutableArray* list = [[NSMutableArray alloc]init];
+                     
+                     bool skip = YES;
+                     
+                     for(NSObject* key in [urlParams allKeys])
+                     {
+                         if(skip)
+                         {
+                             skip = NO;
+                             continue;
+                         }
+
+                             [list addObject:[NSString stringWithFormat:@"%@@facebook.com",
+                              urlParams[key]]];
+
+                     }
+                     
+                     
+                   //  NSLog();
+                     /*
+                     MFMailComposeViewController *mailComposer;
+                     mailComposer  = [[MFMailComposeViewController alloc] init];
+                     mailComposer.mailComposeDelegate = self;
+                     [mailComposer setModalPresentationStyle:UIModalPresentationFormSheet];
+                     [mailComposer setSubject:@"團購小圈圈"];
+                     [mailComposer setToRecipients:list];
+                     [mailComposer setMessageBody:[NSString stringWithFormat:@"來參加我的團購：scc://%@", self.ActivityID] isHTML:NO];
+                     [self presentModalViewController:mailComposer animated:YES];
+
+                     */
+                 }
+             }
+         }
+     }];
+
+}
+
+
+- (void)mailComposeController:(MFMailComposeViewController*)controller
+          didFinishWithResult:(MFMailComposeResult)result
+                        error:(NSError*)error
+{
+    if(error) NSLog(@"ERROR - mailComposeController: %@", [error localizedDescription]);
+    [self dismissModalViewControllerAnimated:YES];
+    return;
+}
+
+- (NSDictionary*)parseURLParams:(NSString *)query {
+    NSArray *pairs = [query componentsSeparatedByString:@"&"];
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    for (NSString *pair in pairs) {
+        NSArray *kv = [pair componentsSeparatedByString:@"="];
+        NSString *val =
+        [kv[1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        params[kv[0]] = val;
+    }
+    return params;
 }
 
 
